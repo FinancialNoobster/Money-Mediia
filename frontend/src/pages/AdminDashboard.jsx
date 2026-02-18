a376c7824e767eedfe37de16260261a527b031bd
FinancialNoobster <viranchgupta1@gmail.com>
2026-02-18 14:23:15 +0530

    Admin Summary dashboard & SummaryCard Careted

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAuth } from '../context/authContext.jsx'
 import AdminSideBar from '../components/dashboard/AdminSideBar.jsx'
 import Navbar from '../components/dashboard/Navbar.jsx'
+import AdminSummary from '../components/dashboard/AdminSummary.jsx'
 
 const AdminDashboard = () => {
   const {user, logout} = useAuth()
@@ -11,6 +12,7 @@ const AdminDashboard = () => {
       <AdminSideBar />
       <div className='flex-1 ml-64 h-screen'>
         <Navbar />
+        <AdminSummary />
       </div>
     </div>
   )
