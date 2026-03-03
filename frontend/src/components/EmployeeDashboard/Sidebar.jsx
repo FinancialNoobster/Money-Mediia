ff28b40c9bf25101e009cbaef912a49c4903c856
FinancialNoobster <viranchgupta1@gmail.com>
2026-03-03 22:16:04 +0530

    Code for Leaves of Individual Employee only RAFCE Code

--- frontend/src/components/EmployeeDashboard/Sidebar.jsx
+++ frontend/src/components/EmployeeDashboard/Sidebar.jsx
@@ -22,7 +22,7 @@ const SideBar = () => {
             <FaUsers />
             <span>My Profile</span>
         </NavLink>
-        <NavLink to="/employee-dashboard/leaves"
+        <NavLink to={`/employee-dashboard/leaves/${user._id}`}
          className={({isActive}) => `${isActive ? "bg-teal-500 " : " "}flex items-center space-x-5 block py-2.5 px-4 rounded `}>
             <FaBuilding />
             <span>Leaves</span>
