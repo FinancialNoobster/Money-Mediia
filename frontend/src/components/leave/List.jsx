0c0c353134309429214556553ae08d522fb10e20
FinancialNoobster <viranchgupta1@gmail.com>
2026-03-08 17:35:35 +0530

    Leave Table in Employee Dashboard (Frontend Code - Design & Structure)

--- frontend/src/components/leave/List.jsx
+++ frontend/src/components/leave/List.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../context/authContext';
+import axios from 'axios';
+
 
 
 const List = () => {
+    const {user} = useAuth()
+    const [leaves, setLeaves] = useState([])
+
+
+
+    const fetchLeaves = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/leave/${user._id}`, {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`
+                },
+            });
+
+            if (response.data.success) {
+                setLeaves(response.data.leaves);
+            }
+        } catch (error) {
+            if (error.response && !error.response.data.success){
+                alert(error.message)
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchLeaves();
+    }, []);
+
   return (
     <div className='p-5'>
         <div className='text-center '>
@@ -12,6 +42,35 @@ const List = () => {
         <input type="text" placeholder='Search By Employee Name' className='px-4 py-0.5 border'/>
         <Link to="/employee-dashboard/add-leave" className='px-4 py-1 bg-gray-500 hover:bg-gray-400 rounded text-white'>
         Add New Leaves</Link>
+      </div>
+      <div>
+        <table className="w-full text-sm text-left text-gray-500">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 border border-gray-200">
+                            <tr>
+                                <th className="px-6 py-3">SNO</th>
+                                <th className="px-6 py-3">Leave Type</th>
+                                <th className="px-6 py-3">From</th>
+                                <th className="px-6 py-3">To</th>
+                                <th className="px-6 py-3">Description</th>
+                                <th className="px-6 py-3">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {leaves.map((leave) => (
+                                <tr
+                                    key={leave._id}
+                                    className="bg-white border-b "
+                                >
+                                    <td className="px-6 py-3">{sno++}</td>
+                                    <td className="px-6 py-3">{leave.leaveType}</td>
+                                    <td className="px-6 py-3">{new Date(leave.startDate).toLocaleDateString()}</td>
+                                    <td className="px-6 py-3">{new Date(leave.endDate).toLocaleDateString()}</td>
+                                    <td className="px-6 py-3">{leave.reason}</td>
+                                    <td className="px-6 py-3">{leave.status}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
       </div>
     </div>
   )
